396168dbe4d6a53bb480680ea0adfe29885b9614
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2022-09-26 18:56:36 -0300

    Transformando a aplicação React em TypeScript React

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,16 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../components/styles/repositories.scss'
 
 
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 // https://api.github.com/users/matheusmfl/repos
 export function RepositoryList(){
 
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
     //cuidado pra não deixar sem o segundo parâmetro o useEffect
     useEffect(()=> {
         fetch('https://api.github.com/users/matheusmfl/repos')
@@ -23,8 +29,7 @@ export function RepositoryList(){
                 {repositories.map(repository => {
                     return <RepositoryItem key={repository.name} repository={repository} />
                 })}
-            <RepositoryItem
-             repository = {repositories}/>
+            
             
 
             </ul>
